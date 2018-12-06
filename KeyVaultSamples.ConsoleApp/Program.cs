1cf61192833c40fb59c3d6e33c1def560a73ac2e
Yuta Matsumura <y-matsumura@alterbooth.com>
2018-12-07 06:48:36 +0900

    Create console app project

---
+++ KeyVaultSamples.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KeyVaultSamples.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
